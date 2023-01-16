28ac3a3e7c301f0fff6fe472c8ee1027e9216bf4
pranay <prana4saini@gmail.com>
2023-01-16 18:58:38 +0530

    Diffutil

---
+++ app/src/main/java/com/pranay/newsapp/adapter/NewsAdapter.kt
@@ -0,0 +1,37 @@
+package com.pranay.newsapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.pranay.newsapp.model.Article
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ArticleViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this,differCallback)
+}
